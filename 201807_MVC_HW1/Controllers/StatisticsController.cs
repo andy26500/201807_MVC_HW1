0496d987ac64b21b3a743a07c8b80ebb7018dad5
AndyLiao <AndyLiao@ANDY-PC>
2018-07-11 16:37:32 +0800

    實作一個清單頁面，顯示欄位有「客戶名稱、聯絡人數量、銀行帳戶數量」共三個欄位

---
+++ 201807_MVC_HW1/Controllers/StatisticsController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using _201807_MVC_HW1.Models;
+
+namespace _201807_MVC_HW1.Controllers
+{
+    public class StatisticsController : Controller
+    {
+        private 客戶統計資訊Repository statisticRepo;
+
+        public StatisticsController()
+        {
+            statisticRepo = RepositoryHelper.Get客戶統計資訊Repository();
+        }
+        
+        // GET: Statistics
+        public ActionResult Index()
+        {
+            var data = statisticRepo.All().ToList();
+            return View(data);
+        }
+    }
+}
